325df76768c013d5c373546b8c357c2df2f8f9cd
LouisSEC <82223115+LouisSEC@users.noreply.github.com>
2022-08-15 17:08:38 -0700

    Update Control_V3.py

--- src/robot_control/robot_control/Control_V3.py
+++ src/robot_control/robot_control/Control_V3.py
@@ -4,22 +4,14 @@ import socket
 class RobotControl:
     S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # LIFT_SPEED: int
-    # LIFT_CTRL: int
-    # LEFT_MOTOR_SPEED: int
-    # RIGHT_MOTOR_SPEED: int
-    # MOTOR_CTRL: int
-    # DATA: int
-    motor_ctrl_state = 0
-
     def __init__(self):
         self.lift_speed: int = 0  # 0 -> stop, 250 -> run
-        self.lift_ctrl:int = 0  # 0 -> nothing, 1 -> down, 2 -> up
-        self.left_motor_speed:int = 0  # 0
-        self.right_motor_speed:int = 0  # 0
+        self.lift_ctrl: int = 0  # 0 -> nothing, 1 -> down, 2 -> up
+        self.left_motor_speed: int = 0  # 0
+        self.right_motor_speed: int = 0  # 0
         self.motor_left_ctrl: int = 0  # 0 left cw, 1 = left ccw
         self.motor_right_ctrl: int = 0  # 0 right cw, 2 = right ccw
-
+        self.motor_ctrl_state: int = 0
 
         self.data = [0x08, 0x00, 0x00, 0x00, 0x12, 0x00, self.lift_speed, self.lift_ctrl, 0x00,
                      abs(self.left_motor_speed) + 60, abs(self.right_motor_speed) + 60,
@@ -54,65 +46,62 @@ class RobotControl:
         ###############
         # comparing current command with new command
         if self.right_motor_speed >= 0 & right_motor < 0:
-            motor_ctrl_state = 0
+            self.motor_ctrl_state = 0
 
         if self.right_motor_speed <= 0 & right_motor > 0:
-            motor_ctrl_state = 0
+            self.motor_ctrl_state = 0
 
         if self.left_motor_speed >= 0 & left_motor < 0:
-            motor_ctrl_state = 0
+            self.motor_ctrl_state = 0
 
         if self.left_motor_speed <= 0 & left_motor > 0:
-            motor_ctrl_state = 0
+            self.motor_ctrl_state = 0
 
         # current command = new command
         self.right_motor_speed = right_motor
-        self.left_motor_speed = right_motor
+        self.left_motor_speed = left_motor
 
-        match motor_ctrl_state:
-            case 0:
-                # stop the motors
-                self.data[9] = 60
-                self.data[10] = 60
+        if self.motor_ctrl_state == 0:
+            # stop the motors
+            self.data[9] = 60
+            self.data[10] = 60
 
-            case 1:
-                # reset direction
-                self.data[11] = 0
+        if self.motor_ctrl_state == 1:
+            # reset direction
+            self.data[11] = 0
 
-            case 2:
-                # change direction
+        if self.motor_ctrl_state == 2:
+            # change direction
 
-                # matching data to control motor and speed data
-                # between 0 and 180
-                if self.left_motor_speed >= 0:
-                    self.motor_left_ctrl = 0
-                # between -180 and -0.1
-                else:
-                    self.motor_left_ctrl = 1
+            # matching data to control motor and speed data
+            # between 0 and 180
+            if self.left_motor_speed >= 0:
+                self.motor_left_ctrl = 1
+            # between -180 and -0.1
+            else:
+                self.motor_left_ctrl = 0
 
-                if self.right_motor_speed >= 0:
-                    self.motor_right_ctrl = 0
-                else:
-                    self.motor_right_ctrl = 2
+            if self.right_motor_speed >= 0:
+                self.motor_right_ctrl = 2
+            else:
+                self.motor_right_ctrl = 0
 
-                # implementing the data
-                self.data[11] = self.motor_left_ctrl + self.motor_right_ctrl
+            # implementing the data
+            self.data[11] = self.motor_left_ctrl + self.motor_right_ctrl
 
-            # default case (normal)
-            case _:
-                # we have to change direction 3 times to make it accurate
-                if motor_ctrl_state > 4:
-                    self.data[9] = abs(self.left_motor_speed) + 60
-                    self.data[10] = abs(self.right_motor_speed) + 60
-                print("a")
+        # default case (normal)
+        if self.motor_ctrl_state > 4:
+            # we have to change direction 3 times to make it accurate
+            self.data[9] = abs(self.left_motor_speed) + 60
+            self.data[10] = abs(self.right_motor_speed) + 60
 
         if self.left_motor_speed == 0:
             self.data[9] = 0
         if self.right_motor_speed == 0:
             self.data[9] = 0
         # increase case
-        if motor_ctrl_state < 10:
-            motor_ctrl_state += 1
+        if self.motor_ctrl_state < 10:
+            self.motor_ctrl_state += 1
 
     def sending_data(self):
         self.S.send(bytes(self.data))
